8119ee784bcf9d6b01f055ab3f0092b0f6f411ef
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-03-28 00:58:56 +0545

    Using inner property of print function
    Using the inner property of the print function to print in different ways

---
+++ print function .py
@@ -0,0 +1,9 @@
+#Prints 2 different print function on different lines 
+name=input("Name= ");
+print("Name is "+name,);
+print("This is in different lines");
+
+#Prints 2 different print function on same line
+
+print("Name is "+name,end=' ');
+print("This is on same line");
