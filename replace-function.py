db67855f9ab77936b1180095ad239915e892481d
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-03-31 14:43:52 +0545

    Using the replace function in Python
    Using the replace function in Python to replace word strings

---
+++ replace-function.py
@@ -0,0 +1,8 @@
+#Using replace function to replace the string value..........
+
+def change(inputs):
+              print(inputs.replace("Hello","Welcome to the jungle")); 
+
+welcome_name="Hello,Mougli";
+print(welcome_name);
+change(welcome_name);
