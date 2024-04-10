541c08dc9e59abe592ea6cddab1553e0fcc7af4e
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-10 22:07:02 +0545

    One of the Looping Technique in Python
    One of the Looping Technique in Python

---
+++ Loop.py
@@ -0,0 +1,15 @@
+
+def meow(n):
+          for _ in range(n):
+               print("*");
+               
+               
+               
+               
+def main():
+          num=input("Enter number=");
+          num=int(num);
+          meow(num);
+          
+
+main();
