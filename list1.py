5bac5f14dca4b2f9928418273355c255d0e5c801
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-12 12:56:59 +0545

    Using List in Python
    Using List in Python

---
+++ list1.py
@@ -0,0 +1,7 @@
+students=["Aadit","Bardan","Max","Henry","Carthford"];
+
+for student in students:
+                    print(student);
+                    
+for i in range(len(students)):
+               print(f"{i}.{students[i]}");
