b5eafe193a2e76adea802f6e8a8cbcb2208af370
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-14 00:02:28 +0545

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 def add(num1,num2):
                result=num1+num2;
                return result;
-
+#to subtract two numbers
 def subtract(num1,num2):
                result=num1-num2;
                return result;
