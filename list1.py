908455716324282f0ff62433791f731019107c17
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-18 14:30:02 +0545

    list1.py

--- list1.py
+++ list1.py
@@ -3,7 +3,7 @@ house=["Red","Green","Blue","Yellow","Purple"];
 
 for student in students:
                     print(student);
-                    
+#To print students along with their respective houses......                 
 for i in range(len(students)):
                print(f"{i+1}.{students[i]} belongs to {house[i]} house.");
                
