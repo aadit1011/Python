e5b3a51c1c5b65ce2eb6339c12d431eea5944567
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-22 21:33:20 +0545

    Simple Exception.py

--- Simple Exceptioon.py
+++ Simple Exceptioon.py
@@ -3,3 +3,11 @@ try:
         print(f"x value is {num}");
 except ValueError:
         print("Please enter a integer value....");
+#Also using else along 
+try:
+        num=int(input("Enter value of x?="));
+except ValueError:
+        print("Please enter a integer value....");
+else:
+        print(f"x value is {num}");
+        
