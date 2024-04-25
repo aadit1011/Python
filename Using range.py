a707750acdda18cd4b5ff8f1ab9fb8ecc04fd06a
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-25 21:10:18 +0545

    Using range.py

---
+++ Using range.py
@@ -0,0 +1,13 @@
+#Upto the maximum iteration count
+for _ in range(5):
+          print(_);
+
+#range(start,stop)
+for i in range(0,5):
+          print(i);
+     
+
+#range(start,stop,step)
+for i in range(1,10,4):
+     print(i);
+  
