f84aa1f9ff6e10d2441829592d830f7e1e090f74
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-25 16:31:22 +0545

    Playing with Strings.py

--- Playing with Strings.py
+++ Playing with Strings.py
@@ -4,3 +4,8 @@ print(first_name, last_name, address, life,sep=", ");
 detail=detail.replace("Pepsicola", "Dubai-AbuDhabi");
 first_name, last_name, address, life=detail.split(" ");
 print(first_name, last_name, address, life,sep=", ");
+first_name=first_name.capitalize();
+print(first_name,sep=', ');
+
+last_name=last_name.capitalize();
+print(last_name,sep=', ');
