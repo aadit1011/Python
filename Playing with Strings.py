b44a9745758d7875f3a3e07d2d0f4967e5cde156
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-25 15:46:22 +0545

    Strings in Python
    Strings in Python

---
+++ Playing with Strings.py
@@ -0,0 +1,6 @@
+detail='Aadit Sharma Pepsicola DataScience&Dance';
+first_name, last_name, address, life=detail.split(" ");
+print(first_name, last_name, address, life,sep=", ");
+detail=detail.replace("Pepsicola", "Dubai-AbuDhabi");
+first_name, last_name, address, life=detail.split(" ");
+print(first_name, last_name, address, life,sep=", ");
