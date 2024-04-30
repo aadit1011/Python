735a9623098cb992646aaadcc2039be49a78b7d6
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-30 21:28:49 +0545

    Tuples Different forms
    Tuples Different forms

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+#Tuple with integers
+tuple0=(1,2,3,4,5,6,7);
+#Tuple with characters
+tuple1=('Harry','Ramesh','Rahul','Rohit','Max','Ron');
+#Mixed Tuples
+tuple2='Aadit Sharma',210108,'Bhaktapur';
+tuple3=('Harry',{'California','Boca Raton','Bel Air',1,2,3,4,5,6,7},'Ramesh',{'Dubai','Singapore','Nepal','Thailand',10,11,12});
+#Nested Tuples
+tuple4='Python',{1,2,3,45,6,7,8,9,10,11},('Aadit Sharma',2101020,'Bhaktapur')
+
+print('Tuple with integer',tuple0)
+print('Tuple with Characters',tuple1);
+print('Mixed Tuple',tuple2);
+print('Mixed Tuple',tuple3);
+print('Nested Tuple',tuple4);
