d752acf9e3670c7fc05731e8c34a6cf085d4a23f
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-02 18:01:23 +0545

    Checking membership in tuple
    Checking membership in tuple

---
+++ Checking membership in tuple.py
@@ -0,0 +1,11 @@
+#Tuples Membership test
+membership_in_zym=('Aadit','Bardan','Bipin','Biyash','Ankit','Roshan');
+small=[];
+
+name=input('Enter the name to check membership for')
+name=name.capitalize();
+value=(name in membership_in_zym);
+if value==True:
+     print(f'{name} is a member of the membership list.')
+else:
+     print(f'{name} is not a member of the membership list.')
