71dac68854b58f1ac938657f38f153ecdf28c107
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-03 17:29:31 +0545

    Playing with Strings.py

--- Playing with Strings.py
+++ Playing with Strings.py
@@ -1,9 +1,11 @@
 detail='Aadit Sharma Pepsicola DataScience&Dance';
+#To split the string when white space
 first_name, last_name, address, life=detail.split(" ");
 print(first_name, last_name, address, life,sep=", ");
 detail=detail.replace("Pepsicola", "Dubai-AbuDhabi");
 first_name, last_name, address, life=detail.split(" ");
 print(first_name, last_name, address, life,sep=", ");
+#To capitalize the string 
 first_name=first_name.capitalize();
 print(first_name,sep=', ');
 
