ff709d270a30dd6bd4a3ee924129bc34f0e65df0
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-05 17:40:02 +0545

    Checking membership in tuple.py

--- Checking membership in tuple.py
+++ Checking membership in tuple.py
@@ -4,6 +4,7 @@ small=[];
 
 name=input('Enter the name to check membership for')
 name=name.capitalize();
+#Using value in tuple way to find membership
 value=(name in membership_in_zym);
 if value==True:
      print(f'{name} is a member of the membership list.')
