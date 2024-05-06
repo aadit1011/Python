1e7b70bb3b5b70a0d48299e9f59f2109c3d76b86
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-06 15:49:29 +0545

    Intersection in set.py

--- Intersection in set.py
+++ Intersection in set.py
@@ -3,3 +3,8 @@ set1={'Aadit','Bipin','Biyash'};
 set2={'Aadit','Ankit','Roshan'};
 set3=set1.intersection(set2);
 print(set3);
+
+#another way for set union
+seta={'Aadit Sharma','Bardan Babu Shiwakoti','Dubai','Boca Raton'};
+setb={'Aadit Sharma','Dubai','Boca Raton','Bel Air','Miami','Switzerland','Beverly Hills'};
+print(seta&setb);
