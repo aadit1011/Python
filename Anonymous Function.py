137e183bf0149956c3bb03d8cfb81008fdfee26e
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-09 20:32:05 +0545

    Anonymous Function
    Anonymous Function

---
+++ Anonymous Function.py
@@ -0,0 +1,13 @@
+#Using anonymous function
+def main():  
+     number=int(input('Please!Enter a number='));
+     num=int(input('Please Enter another number='));
+     add=lambda number, num:number+num;
+     sub=lambda number,num:number-num;
+     mul=lambda number,num:number*num; 
+     print('Add=',add(number,num));
+     print('Sub=',sub(number,num));
+     print('Mul=',mul(number,num));
+     
+     
+main();
