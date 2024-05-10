a9632fae197c7937c3d9f387575753a5a91f49e3
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-10 17:07:25 +0545

    Built in function
    Built in function

---
+++ Built-in Function.py
@@ -0,0 +1,31 @@
+#Using absolute function
+#abs()
+def main():
+     num=int(input("Enter number="));
+     numb=abs(num);
+     print(f'The absolute value of {num} is {numb}');
+     
+main();
+
+
+#Using eval function
+#eval()
+
+def main():
+     operation=input('Please Input the mathematical operation you want to perform=');
+     operation=eval(operation);
+     print(f'The final result of the operation you performed is:{operation}')
+     
+main();
+
+#Using exec function
+#exec()
+
+operation1='''
+num1=int(input('Enter the a='));
+num2=int(input('Enter the b='));
+sum=num1+num2;
+print(f'The sum of two numbers {num1} and {num2} is:{sum}')
+''';
+exec(operation1);
+
