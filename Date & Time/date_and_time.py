8f97f084a4a3df3f93d4f68ebb1b35050814ae9e
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-29 20:53:16 +0545

    Date and time
    Date and time

---
+++ Date & Time/date_and_time.py
@@ -0,0 +1,15 @@
+import datetime;
+import time;
+#Sleep helps to execute the certain loop after the provided time...
+for i in range(5):
+     print(i);
+     #Here time is 1 sec
+     time.sleep(1);
+
+#helps to print the date and time of this moment.....
+print(datetime.datetime.now());
+
+
+
+
+
