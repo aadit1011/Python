928a2a7d3930628021eb8cf5bb279a3c04263ed5
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-06-09 23:18:53 +0545

    array function
    array function

---
+++ NumPy/array_function.py
@@ -0,0 +1,8 @@
+# Importing the numpy library using the alias 'np'
+import numpy as np;
+
+# Creating a 2D numpy array by passing a list of lists (each inner list represents a row)
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+
+# Printing the numpy array
+print(a);
