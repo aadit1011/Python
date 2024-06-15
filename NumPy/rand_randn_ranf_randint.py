fa826e9e81a02a6744d23e65b2ae7390b87bbecf
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-06-15 21:55:51 +0545

    rand , randint, ranf, randn  functions
    rand , randint, ranf, randn  functions

---
+++ NumPy/rand_randn_ranf_randint.py
@@ -0,0 +1,29 @@
+#Using rand function
+import numpy as np;
+print('rand function');
+arr_rand=np.random.rand(4);
+print(arr_rand);
+print();
+arr_rand2=np.random.rand(3,4);
+print(arr_rand2);
+
+#Using randn function
+print(" ");
+print("randn function");
+arr_randn=np.random.randn(4);
+print(arr_randn);
+print(" ");
+arr_rand2=np.random.randn(3,4);
+print(arr_rand2);
+
+#Using randint function
+print();
+print('randint function');print(" ");
+arr_randint=np.random.randint(1,10,4);
+print(arr_randint);
+
+#Using ranf function
+print();
+print('ranf function');
+arr_ranf=np.random.ranf(4);
+print(arr_ranf);print(" ");
