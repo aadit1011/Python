ea0edacd255e3f1adcae48eb6e82a5a7b174a88a
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-07-06 07:48:23 +0545

    ndim,shape_and_size_function.py

---
+++ NumPy/ndim,shape_and_size_function.py
@@ -0,0 +1,62 @@
+import numpy as np  # Importing the numpy library for numerical computations and array operations.
+
+# Creating a 3D NumPy array.
+arr3d = np.array([
+                [[1, 2, 3],
+                [4, 5, 6]]
+                ])
+
+# Printing the original 3D array.
+print(f'The original 3D array = {arr3d}')
+print()  # Print an empty line for readability.
+
+# Printing the size of the 3D array (total number of elements).
+print(f'The size of 3D array is {arr3d.size}')
+print()  # Print an empty line for readability.
+
+# Printing the number of dimensions of the 3D array.
+print(f'The dimension of array is {arr3d.ndim}')
+print()  # Print an empty line for readability.
+
+# Printing the shape of the 3D array (dimensions of the array).
+print(f'The shape of array is {arr3d.shape}')
+print()  # Print an empty line for readability.
+
+# Creating a 1D NumPy array.
+arr1d = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+# Printing the 1D array.
+print(f'The 1 dimensional array is = {arr1d}')
+print()  # Print an empty line for readability.
+
+# Printing the size of the 1D array (total number of elements).
+print(f'The size of 1D array is {arr1d.size}')
+print()  # Print an empty line for readability.
+
+# Printing the number of dimensions of the 1D array.
+print(f'The dimension of array is {arr1d.ndim}')
+print()  # Print an empty line for readability.
+
+# Printing the shape of the 1D array (dimensions of the array).
+print(f'The shape of array is {arr1d.shape}')
+print()  # Print an empty line for readability.
+
+# Creating a 2D NumPy array.
+arr2d = np.array([[1, 2, 3],
+                  [4, 5, 6]])
+
+# Printing the 2D array.
+print(f'The 2D array is = {arr2d}')
+print()  # Print an empty line for readability.
+
+# Printing the size of the 2D array (total number of elements).
+print(f'The size of 2D array is {arr2d.size}')
+print()  # Print an empty line for readability.
+
+# Printing the number of dimensions of the 2D array.
+print(f'The dimension of array is {arr2d.ndim}')
+print()  # Print an empty line for readability.
+
+# Printing the shape of the 2D array (dimensions of the array).
+print(f'The shape of array is {arr2d.shape}')
+print()  # Print an empty line for readability.
