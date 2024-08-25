2634e451240d75eb1982c8ac65a652ec2fab59f8
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-08-25 23:14:47 +0545

    Using_seed_function_to_generate_constant_number.py

---
+++ NumPy/Using_seed_function_to_generate_constant_number.py
@@ -0,0 +1,10 @@
+import numpy as np  # Import the NumPy library for numerical operations
+
+# Set the seed for the random number generator to ensure reproducibility
+np.random.seed(1)
+
+# Generate an array 'v' of 31 random integers between 10 (inclusive) and 20 (exclusive)
+v = np.random.randint(10, 20, 31)
+
+# Print the generated array 'v'
+v
